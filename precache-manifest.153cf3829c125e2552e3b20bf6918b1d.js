476af6e7115b12d631a3fce65183c7d367e3e7a0
SantiagoCuinas <Santiago.cuinas@gmail.com>
2020-10-02 19:37:04 -0300

    Updates

---
+++ precache-manifest.153cf3829c125e2552e3b20bf6918b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df73c83289fab46b6759858c9ffd6cdd",
+    "url": "/Led-test/index.html"
+  },
+  {
+    "revision": "91056139d3713c6284bc",
+    "url": "/Led-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/Led-test/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Led-test/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91056139d3713c6284bc",
+    "url": "/Led-test/static/js/main.b25f4436.chunk.js"
+  },
+  {
+    "revision": "68c03ff63fde05057231",
+    "url": "/Led-test/static/js/runtime-main.d464987e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Led-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
