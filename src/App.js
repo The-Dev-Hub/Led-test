cccb73fac887e7c5de09264961e1fed33b9d69bd
SantiagoCuinas <Santiago.cuinas@gmail.com>
2020-10-02 19:29:25 -0300

    Update app js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function fire(led) {
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+
+    // get a callback when the server responds
+    xhr.addEventListener("load", () => {
+      // update the state of the component with the result here
+      console.log(xhr.responseText);
+    });
+    // open the request with the verb and the url
+    xhr.open(
+      "GET",
+      "https://5f7789a49449f700070e1767.onlosant.com/gpio/" + led
+    );
+    // send the request
+    xhr.send();
+  }
+
+  function fireRed(e) {
+    e.preventDefault();
+    console.log("The link was clicked.");
+    fire(17);
+  }
+
+  function fireYellow(e) {
+    e.preventDefault();
+    console.log("The link was clicked.");
+    fire(27);
+  }
+
+  function fireGreen(e) {
+    e.preventDefault();
+    console.log("The link was clicked.");
+    fire(22);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={fireRed}>
+        RED
+      </button>
+      <button type="button" onClick={fireYellow}>
+        YELLOW
+      </button>
+      <button type="button" onClick={fireGreen}>
+        GREEN
+      </button>
     </div>
   );
 }
-
 export default App;
